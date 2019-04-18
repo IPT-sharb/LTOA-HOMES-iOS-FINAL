74a961c78a0408b184a928b0354df366e23b609d
Stephen Harb <stephen.harb@ironpixietechnologies.com>
2019-04-17 23:21:36 -0500

    Initial Commit

---
+++ LTOA-HOMESTests/LTOA_HOMESTests.swift
@@ -0,0 +1,34 @@
+//
+//  LTOA_HOMESTests.swift
+//  LTOA-HOMESTests
+//
+//  Created by Stephen Harb on 4/17/19.
+//  Copyright © 2019 Stephen Harb. All rights reserved.
+//
+
+import XCTest
+@testable import LTOA_HOMES
+
+class LTOA_HOMESTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
