47f397638fba14b4d80f69ba5ac6dfc1065a3e84
Stephen Harb <stephen.harb@ironpixietechnologies.com>
2019-04-19 00:00:16 -0500

    commit for personal data change

--- LTOA-HOMES/ViewControllerMain.swift
+++ LTOA-HOMES/ViewControllerMain.swift
@@ -13,6 +13,9 @@ class ViewControllerMain: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var memberName: UILabel!
     
     var Name: String?
+    var Address: String?
+    var Number: String?
+    var Email: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +27,55 @@ class ViewControllerMain: UIViewController, UITextFieldDelegate {
     {
         self.view.endEditing(true)
     }
-}
+    
+    @IBAction func personalInfoButton(_ sender: Any) {
+        //self.performSegue(withIdentifier: "mainToPersonal" , sender: nil)
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/search"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberName": Name]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from Member data")
+            return
+        }
+        
+        exec(request: request, session: session)
+        self.performSegue(withIdentifier: "mainToPersonal" , sender: nil)
+        
+    }
+    
+    func exec(request: URLRequest, session: URLSession)
+    {
+        let sem = DispatchSemaphore.init(value: 0)
+        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) in
+            let json = try? JSONSerialization.jsonObject(with: (data ?? nil)!, options: []) as! [String: AnyObject]
+            let jsonBodyAddress = json?["memberAddress"] as? String
+            let jsonBodyNumber = json?["contactNumber"] as? String
+            let jsonBodyEmail = json?["email"] as? String
+            self.Address = jsonBodyAddress
+            self.Number = jsonBodyNumber
+            self.Email = jsonBodyEmail
+            sem.signal()
+        }
+        )
+        task.resume()
+        sem.wait()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let ViewControllerPersonal = segue.destination as? ViewControllerPersonal {
+            ViewControllerPersonal.loginName = Name
+            ViewControllerPersonal.address = Address
+            ViewControllerPersonal.email = Email
+            ViewControllerPersonal.number = Number
+        }
+    }}
--- LTOA-HOMES/ViewControllerPersonal.swift
+++ LTOA-HOMES/ViewControllerPersonal.swift
@@ -17,11 +17,24 @@ class ViewControllerPersonal: UIViewController, UITextFieldDelegate  {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var addressField: UITextField!
     @IBOutlet weak var numberField: UITextField!
-    
     @IBOutlet weak var emailField: UITextField!
+    
+    var loginName: String?
+    var address: String?
+    var number: String?
+    var email: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addressField.delegate = self
+        numberField.delegate = self
+        emailField.delegate = self
         
+        memberNameText.text = loginName
+        addressText.text = address
+        numberText.text = number
+        emailText.text = email 
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -29,13 +42,93 @@ class ViewControllerPersonal: UIViewController, UITextFieldDelegate  {
         self.view.endEditing(true)
     }
     @IBAction func homeButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "personalToMain" , sender: nil)
     }
-    @IBAction func changeName(_ sender: Any) {
-    }
+
     @IBAction func changeAddress(_ sender: Any) {
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/update"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberName": loginName, "memberAddress": addressField.text]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from Member data")
+            return
+        }
+        
+        exec(request: request, session: session)
+        addressText.text = addressField.text
+        addressField.text = nil
     }
     @IBAction func changeNuber(_ sender: Any) {
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/update"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberName": loginName, "contactNumber": numberField.text]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from Member data")
+            return
+        }
+        
+        exec(request: request, session: session)
+        numberText.text = numberField.text
+        numberField.text = nil
     }
     @IBAction func changeEmail(_ sender: Any) {
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/update"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberName": loginName, "email": emailField.text]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from Member data")
+            return
+        }
+        
+        exec(request: request, session: session)
+        emailText.text = emailField.text
+        emailField.text = nil
+        
     }
+    
+    func exec(request: URLRequest, session: URLSession)
+    {
+        let sem = DispatchSemaphore.init(value: 0)
+        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) in
+            sem.signal()
+        }
+        )
+        task.resume()
+        sem.wait()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let ViewControllerMain = segue.destination as? ViewControllerMain {
+            ViewControllerMain.Name = loginName
+        }}
 }
