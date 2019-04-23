c3cff9170c3fb94aa380c482ff9e64056a59031b
Stephen Harb <stephen.harb@ironpixietechnologies.com>
2019-04-22 22:20:36 -0500

    Finale!

--- LTOA-HOMES/ViewControllerSpecialInstructions.swift
+++ LTOA-HOMES/ViewControllerSpecialInstructions.swift
@@ -10,19 +10,105 @@ import UIKit
 
 class ViewControllerSpecialInstructions: UIViewController, UITextFieldDelegate, UITextViewDelegate {
     
+    var loginName: String?
+    var address: String?
+    
     @IBOutlet weak var specialInstructionsStaticText: UITextView!
     
     @IBOutlet weak var editSpecialInstructions: UITextView!
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        specialInstructionsStaticText.text = ""
+        specialInstructionsStaticText.delegate = self
+        searchSpecialInstructions()
+    }
     
     @IBAction func homeFromSpecialInstructions(_ sender: Any) {
-        //specialInstructionsHome
+        self.performSegue(withIdentifier: "specialInstructionsHome" , sender: nil)
+    }
+    
+    func execSearch(request: URLRequest, session: URLSession)
+    {
+        let sem = DispatchSemaphore.init(value: 0)
+        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) in
+            let json = try? JSONSerialization.jsonObject(with: (data ?? nil)!, options: []) as! [[String: AnyObject]]
+            for(index, element) in (json?.enumerated())!
+            {
+                let stringPass = element["specialInstructions"] as? String
+                self.specialInstructionsStaticText.text = stringPass
+                self.editSpecialInstructions.text = nil
+            }
+            sem.signal()
+        }
+        )
+        task.resume()
+        sem.wait()
+    }
+    
+    func searchSpecialInstructions()
+    {
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/instructions/search"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/text; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberAddress": self.address]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from login data")
+            return
+        }
+        
+        execSearch(request: request, session: session)
+    }
+    
+    @IBAction func submitSpecialButton(_ sender: Any) {
+        var endpoint : String = "https://d1jq46p2xy7y8u.cloudfront.net/member/special-instructions/add"
+        let url = URL(string: endpoint)
+        var request = URLRequest(url: url!)
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/text; charset=utf-8", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let loginData: [String?: Any] = ["memberAddress": self.address, "specialInstructions": self.editSpecialInstructions.text]
+        let loginJson: Data
+        do {
+            loginJson = try JSONSerialization.data(withJSONObject: loginData, options: [])
+            request.httpBody = loginJson
+        } catch {
+            print("Error: cannot create JSON from login data")
+            return
+        }
+        
+        execAdd(request: request, session: session)
+        self.performSegue(withIdentifier: "submitSpecialInstructions" , sender: nil)
+    }
+    
+    func execAdd(request: URLRequest, session: URLSession)
+    {
+        let sem = DispatchSemaphore.init(value: 0)
+        let task = session.dataTask(with: request, completionHandler: {(data: Data?, response: URLResponse?, error: Error?) in
+            sem.signal()
+        }
+        )
+        task.resume()
+        sem.wait()
     }
     
     
-    @IBAction func submitSpecialButton(_ sender: Any) {
-        //submitSpecialInstructions
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let ViewControllerMain = segue.destination as? ViewControllerMain {
+            ViewControllerMain.Name = loginName
+        }
+
     }
     
 }
